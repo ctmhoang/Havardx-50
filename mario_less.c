87ecaf14d9ef6dcc986ee8d6747d099aa7428031
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2020-01-21 11:05:43 -0500

    Update mario_less.c

--- mario_less.c
+++ mario_less.c
@@ -1,19 +1,30 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_pos(void);
+
 int main(void)
 {
-    int h = get_int("Height: ");
+    int h = get_pos();
     for(int i = 0; i <h ; i++)
     {
         for(int j = 0 ; j < h-i-1 ; j ++)
         {
                 printf(" ");
-        }
-        for(int j = 0 ; j < i + 1; j ++)
+        }   
+        for(int j = 0 ; j < i+1 ; j ++)
         {
-            printf("#");
-        }
+                printf("#");
+        }  
         printf("\n");
     }
+}
+int get_pos(void)
+{
+    int res;
+    do
+    {
+        res = get_int("Height: ");
+    }while(!(res > 0 && res < 9));
+    return res;
 }
