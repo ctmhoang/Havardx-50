cdde5519ba31542305b08058e899bae20502c864
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2020-01-28 11:34:59 -0500

    Formatted code

--- readability.c
+++ readability.c
@@ -8,7 +8,7 @@ int count_letters(string input);
 int count_words(string input);
 int count_sentences(string input);
 float per_100_words(int input, int words);
-void Coleman_Liau_index (int l, int w, int s);
+void Coleman_Liau_index(int l, int w, int s);
 
 int main(void)
 {
@@ -20,9 +20,9 @@ int count_letters(string input)
 //Letters can be any uppercase or lowercase alphabetic characters, but shouldn’t include any punctuation, digits, or other symbols.
 {
     int count = 0;
-    for(int i = 0 ; i < strlen(input) ; i++)
+    for (int i = 0 ; i < strlen(input) ; i++)
     {
-        if(isalpha(input[i]))
+        if (isalpha(input[i]))
         {
             count ++;
         }
@@ -32,14 +32,14 @@ int count_letters(string input)
 int count_words(string input)
 //assumed that a sentence will not start or end with a space, and a sentence will not have multiple spaces in a row.
 {
-    if(input[0] == ' ' || strcmp("",input) == 0)
+    if (input[0] == ' ' || strcmp("", input) == 0)
     {
         return 0;
     }
     int count = 1;
-    for(int i = 0 ; i < strlen(input) ; i++)
+    for (int i = 0 ; i < strlen(input) ; i++)
     {
-        if(input[i] == ' ')
+        if (input[i] == ' ')
         {
             count ++;
         }
@@ -51,20 +51,20 @@ int count_sentences(string input)
 // considered any sequence of characters that ends with a . or a ! or a ? to be a sentence
 {
     int count = 0;
-    for(int i = 0 ; i < strlen(input) ; i++)
+    for (int i = 0 ; i < strlen(input) ; i++)
     {
-        if(input[i] == '.' || input[i] == '!' || input[i] == '?')
+        if (input[i] == '.' || input[i] == '!' || input[i] == '?')
         {
             count ++;
         }
     }
     return count;
 }
-void Coleman_Liau_index (int l, int w, int s)
+void Coleman_Liau_index(int l, int w, int s)
 {
-    float L = per_100_words(l,w), S = per_100_words(s,w);
+    float L = per_100_words(l, w), S = per_100_words(s, w);
     int index = round(0.0588 * L - 0.296 * S - 15.8);
-    if(index > 16)
+    if (index > 16)
     {
         printf("Grade 16+\n");
     }
@@ -74,13 +74,13 @@ void Coleman_Liau_index (int l, int w, int s)
     }
     else
     {
-        printf("Grade %i\n",index);
+        printf("Grade %i\n", index);
     }
 }
 
 float per_100_words(int input, int words)
 {
-    if(words == 100)
+    if (words == 100)
     {
         return input;
     }
